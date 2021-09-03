38db15678501d945ebb14a5455c79651855c53f1
Chiro <sachinsinghroxx@gmail.com>
2021-09-03 21:15:02 +0530

    added library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DocumentListComponent } from './components/document-list/document-list.component';
+import { DocumentComponent } from './components/document/document.component';
+
+import { FormsModule } from '@angular/forms';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocumentListComponent,
+    DocumentComponent
   ],
   imports: [
     BrowserModule
