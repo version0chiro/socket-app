69f7dc592d31ef0b1532ce58880974e44545cf01
Chiro <sachinsinghroxx@gmail.com>
2021-09-04 16:57:27 +0530

    added a ng model for placeholder

--- src/app/components/chatbox/chatbox.component.ts
+++ src/app/components/chatbox/chatbox.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { startWith } from 'rxjs/operators';
+
+import { Message } from '../../models/message';
+import { DocumentService } from 'src/app/services/document.service';
 
 @Component({
   selector: 'app-chatbox',
   templateUrl: './chatbox.component.html',
-  styleUrls: ['./chatbox.component.scss']
+  styleUrls: ['./chatbox.component.scss'],
 })
 export class ChatboxComponent implements OnInit {
+  currentMessage: string;
+  message: Message;
+  private _docSub: Subscription;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private documentService: DocumentService) {
+    this.currentMessage = '';
+    this.message = new Message();
+    this._docSub = new Subscription();
   }
 
+  ngOnInit(): void {}
+
+  sendMessage() {
+    this.message.content = this.currentMessage;
+    this.documentService.sendMessage(this.message);
+  }
+
+  getMessages(id: string) {
+    this.documentService.getMessages(id);
+  }
 }
